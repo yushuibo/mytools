cb00b0a30a8c27406a85b3e43dc89ce8801305b2
yushuibo <hengchen2005@gmail.com>
2021-04-24 18:27:00 +0800

    update ishadow util

--- ishadow-util.py
+++ ishadow-util.py
@@ -19,8 +19,7 @@ from requests import ConnectionError
 
 servers = []
 
-doc_root = '../../fish-payload'
-payloads_file = '{}/payloads.txt'.format(doc_root)
+payloads_file = 'payloads.txt'
 
 ishadow_url = 'https://my.ishadowx.biz/'
 sssub_prefix_url = 'https://raw.githubusercontent.com/ssrsub/ssr/master/'
@@ -124,8 +123,6 @@ def builder(ss_payloads, vmess_payloads):
 
 def gen_file(servers):
     print('Starting generate subcribe files...')
-    if not os.path.isdir(doc_root):
-        os.mkdir(doc_root)
 
     with open(payloads_file, 'w') as fd:
         fd.write(base64.b64encode('\n'.join(servers).encode()).decode())
