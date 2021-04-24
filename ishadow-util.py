6908037b026a1041751cb2a4aff93f093f22e865
yushuibo <hengchen2005@gmail.com>
2021-04-24 20:41:54 +0800

    update ishadow util

--- ishadow-util.py
+++ ishadow-util.py
@@ -125,7 +125,7 @@ def gen_file(servers):
     index = 1
     for hosts in servers:
         with open('{}_00{}.txt'.format(payloads_prefix_name, index), 'w') as fd:
-            if index == 5:
+            if index > 4:
                 hosts = filter(lambda x: x, hosts)
                 fd.write(base64.b64encode('\n'.join(hosts).encode()).decode())
             else:
